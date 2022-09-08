758ee64af3fa074472bcee40185c86e82e8562fd
marianasantos <gomesmarianasantos78@gmail.com>
2022-09-08 08:34:10 -0300

    introducao a JS

---
+++ anotacoes.js
@@ -0,0 +1,39 @@
+/*
+JAVASCRIPT - CLIENT - SIDE 
+
+HTML É ONDE EU COLOCO O MEU CÓDIGO
+EM JAVASCRIPT.
+
+JS É POPULAR EM MAIS DE 90% DAS PÁGINAS
+DA INTERNET 
+
+CSS É ONDE EU ESTILIZO A MINHA PÁGINA HTML
+
+JAVASCRIPT É ONDE EU DOU FUNCIONALIDADE
+PARA MINHA PÁGINA
+
+
+console.log("") -> Tem a mesma função do "ESCREVA" do Portugol
+
+-----------
+
+DOM - Document Object Model 
+
+É lá que eu consigo estruturar todo o meu documento.
+
+O HTML é o "que" . ON DOM é o "como"
+
+----
+VARIÁVEIS EM JAVASCRIPT
+
+
+Existem 3 formas simples de se declarar variáveis em JS
+
+var & let = para variáveis que mudam com o tempo
+
+
+const = para variáveis que serão sempre constantes
+
+TRIPAGEM DINÂMICA - O Javascript consegue reconhecer automáticamente 
+o tipo de cada váriavel, sem que eu precise especificar 
+nos mínimos detalhes
